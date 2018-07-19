ac705bac7e890e891826d75540f3984221db54b5
Yogesh Mahale <yogesh.bonzer@gmail.com>
2018-07-19 14:59:29 +0530

    On room user connection, update response

--- app/controllers/api/v1/rooms_controller.rb
+++ app/controllers/api/v1/rooms_controller.rb
@@ -6,7 +6,8 @@ class Api::V1::RoomsController < Api::V1::BaseController
     room = Room.find(params[:room_id])
     outcome = Rooms::Connect.run(user: current_user, room: room)
     if outcome.success?
-      head :ok
+      #head :ok
+      render json: { message: 'ok' }, status: 200
     else
       render json: { errors: outcome.errors }, status: :bad_request
     end
