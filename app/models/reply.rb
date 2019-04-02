f1a6cffe2bb77c0239509982f16bb83dd9237aca
Yogesh Mahale <yogesh.bonzer@gmail.com>
2019-04-02 10:53:50 +0530

    Added counter cache on belonging reply modal

--- app/models/reply.rb
+++ app/models/reply.rb
@@ -1,4 +1,4 @@
 class Reply < ApplicationRecord
-  belongs_to :message, required: true
+  belongs_to :message, required: true, counter_cache: :reply_count
   has_many :messages, as: :messageable, dependent: :destroy
 end
