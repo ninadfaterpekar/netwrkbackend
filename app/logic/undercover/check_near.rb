49e97fee08a5555376121b9088959cfcef6bf9ea
Yogesh Mahale <yogesh.bonzer@gmail.com>
2019-05-11 10:56:08 +0530

    Updated check near function criteria

--- app/logic/undercover/check_near.rb
+++ app/logic/undercover/check_near.rb
@@ -23,7 +23,7 @@ class Undercover::CheckNear
     messages = Message.undercover_is(true).with_users
 
     messages.each do |message|
-      next unless (message.lat.to_s[0..3] == current_lat.to_s[0..3]) &&
+      next unless (message.lat.to_s[0..3] == current_lat.to_s[0..3]) ||
                   (message.lng.to_s[0..3] == current_lng.to_s[0..3])
       distance = Geocoder::Calculations.distance_between(
         [current_lng, current_lat], [message.lng, message.lat]
