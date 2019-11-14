0c520de30b01a366da60af38066a80e9bc48ca16
Yogesh Mahale <bonzer.yogesh@gmail.com>
2019-11-14 18:27:22 +0530

    Add method to fetch room users avatar details

--- app/models/rooms_user.rb
+++ app/models/rooms_user.rb
@@ -1,4 +1,9 @@
 class RoomsUser < ApplicationRecord
   belongs_to :user
   belongs_to :room, counter_cache: :users_count
+
+  def user
+    u = User.find_by(id: user_id)
+    u.as_json(methods: %i[id avatar_url hero_avatar_url], only: %i[name role_name])
+  end
 end
