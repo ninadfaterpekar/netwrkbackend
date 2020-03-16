5fea62a1f500c1ab7e69581b121d9366749c1701
Yogesh Mahale <bonzer.yogesh@gmail.com>
2020-03-16 15:58:56 +0530

    Update config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,7 +6,7 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
-  config.action_cable.url = 'ws://api.somvo.app/cable'
+  config.action_cable.url = 'ws://18.188.223.201:3000/cable'
   # Do not eager load code on boot.
   config.eager_load = false
 
